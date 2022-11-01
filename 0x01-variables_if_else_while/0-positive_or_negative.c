014c514e7168dd2204f7c8e02b0fd0412ad6403d
Omaido1994 <omaidodaniel2@gmail.com>
2022-11-01 11:00:24 -0700

    use tabs where possible

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	/* is positive */
- 	if (n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-
 	/* is equal to zero */
 	else if (n == 0)
 		printf("%d is zero\n", n);
-
 	/*is negative */
 	else
 		printf("%d is negative\n", n);
