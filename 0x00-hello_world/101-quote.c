14da59973941e2d327521dd6f80a43c2c4047d76
Omaido1994 <omaidodaniel2@gmail.com>
2022-11-15 05:23:01 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
-int main(void)
+#include <unistd.h>
 
+/**
+ * main - write to stdout
+ * Return: Always 0
+ */
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
